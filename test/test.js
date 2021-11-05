bb26f17f5f6b364fc8324c3893390de6f196abbb
gusgalperin <gusgalper@gmail.com>
2021-11-04 21:47:57 -0300

    tests

--- test/test.js
+++ test/test.js
@@ -8,54 +8,45 @@ const URL = `http://localhost:${PORT}/api`
 
 const dao = getDaoEvents();
 
-await server.conectar()
+let tests = [
+    casoFeliz,
+    casoSinApiKeyAlPedirToken,
+    casoApiKeyErroreaAlPedirToken,
+    casoSinTokenAlPedirRecursoSeguro,
+    casoTokenInvalidoAlPedirRecursoSeguro
+]
 
-await casoFeliz()
+await run(tests)
 
-await borrarTodo()
+async function run(){
+    await server.conectar()
+    
+    for (const test of tests) {
+        await test()
+        await borrarTodo()
 
-await casoSinApiKeyAlPedirToken()
-
-await borrarTodo()
-
-await casoApiKeyErroreaAlPedirToken()
-
-await borrarTodo()
-
-await casoSinTokenAlPedirRecursoSeguro()
-
-await borrarTodo()
-
-await casoTokenInvalidoAlPedirRecursoSeguro()
-
-await borrarTodo()
-
-await server.desconectar()
+        console.log('-------------------------------------------')
+    }
+    
+    await server.desconectar()
+}
 
 async function casoFeliz(){
     console.log('probando caso feliz')
-    console.log('creando evento')
+
     let eventoCreado = await crearEvento();
-    console.log(`evento creado: ${eventoCreado.id}`)
 
-    console.log('obteniendo access token')
     let token = await obtenerAccessToken(eventoCreado.apiKey)
-    console.log(`access token: ${token.access_token}`)
 
-    console.log('obteniendo recurso seguro')
     let evento = await obtenerRecursoSeguro(eventoCreado.id, token.access_token)
-    console.log('recurso seguro obtenido')
-    console.log(evento)
 }
 
 async function casoSinApiKeyAlPedirToken(){
     console.log('probando caso: sin apikey al pedir token')
-    console.log('creando evento')
+
     let eventoCreado = await crearEvento();
-    console.log(`evento creado: ${eventoCreado.id}`)
 
     try {
-        console.log('obteniendo access token sin apikey')
         let token = await obtenerAccessToken('')
     } catch (error) {
         console.log(JSON.stringify(error))
@@ -64,12 +55,10 @@ async function casoSinApiKeyAlPedirToken(){
 
 async function casoApiKeyErroreaAlPedirToken(){
     console.log('probando caso: apikey invalida al pedir token')
-    console.log('creando evento')
+
     let eventoCreado = await crearEvento();
-    console.log(`evento creado: ${eventoCreado.id}`)
 
     try {
-        console.log('obteniendo access token con apikey invalida')
         let token = await obtenerAccessToken(eventoCreado.apiKey.slice(0, -1))
     } catch (error) {
         console.log(JSON.stringify(error))
@@ -78,16 +67,12 @@ async function casoApiKeyErroreaAlPedirToken(){
 
 async function casoSinTokenAlPedirRecursoSeguro() {
     console.log('probando caso: sin token al pedir recurso seguro')
-    console.log('creando evento')
-    let eventoCreado = await crearEvento();
-    console.log(`evento creado: ${eventoCreado.id}`)
 
-    console.log('obteniendo access token')
+    let eventoCreado = await crearEvento();
+
     let token = await obtenerAccessToken(eventoCreado.apiKey)
-    console.log(`access token: ${token.access_token}`)
 
     try {
-        console.log('obteniendo recurso seguro sin token')
         let evento = await obtenerRecursoSeguro(eventoCreado.id, '')
     } catch (error) {
         console.log(JSON.stringify(error))
@@ -96,16 +81,12 @@ async function casoSinTokenAlPedirRecursoSeguro() {
 
 async function casoTokenInvalidoAlPedirRecursoSeguro() {
     console.log('probando caso: token invalido al pedir recurso seguro')
-    console.log('creando evento')
-    let eventoCreado = await crearEvento();
-    console.log(`evento creado: ${eventoCreado.id}`)
 
-    console.log('obteniendo access token')
+    let eventoCreado = await crearEvento();
+
     let token = await obtenerAccessToken(eventoCreado.apiKey)
-    console.log(`access token: ${token.access_token}`)
 
     try {
-        console.log('obteniendo recurso seguro con token invalido')
         let evento = await obtenerRecursoSeguro(eventoCreado.id, token.access_token.slice(0, -1))
     } catch (error) {
         console.log(JSON.stringify(error))
@@ -113,6 +94,8 @@ async function casoTokenInvalidoAlPedirRecursoSeguro() {
 }
 
 async function crearEvento(){
+    console.log('creando evento')
+
     const evento = {
         "codigo": "ARG_CHILE_09-2021",
         "descripcion": "Venta de entradas para el partido de Argentina vs Chile por la fecha 24 de las eliminatorias para la copa del mundo Qatar 2022",
@@ -127,21 +110,31 @@ async function crearEvento(){
 
     let apikeyId = await cliente.post(evento);
 
+    console.log(`evento creado: ${apikeyId.id}`)
+
     return apikeyId;
 }
 
 async function obtenerAccessToken(apikey){
+    console.log(`obteniendo access token - apikey: ${apikey}`)
+
     const cliente = new ClienteHTTP(`${URL}/auth/access-token`)
 
     let token = await cliente.post({apikey: apikey});
+    
+    console.log(`access token: ${token.access_token}`)
 
     return token;
 }
 
 async function obtenerRecursoSeguro(id, token){
+    console.log(`obteniendo recurso seguro - id: ${id} token: ${token}`)
     const cliente = new ClienteHTTP(`${URL}/events/${id}`)
 
     let evento = await cliente.get(null, {'x-access-token': token});
+
+    console.log('recurso seguro obtenido')
+    console.log(evento.id)
 
     return evento;
 }
